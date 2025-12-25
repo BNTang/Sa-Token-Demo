1a1cb220a9ca2b3226eabda639cf620cf94c13c7
Neo <431738916@qq.com>
2025-12-26 00:57:12 +0800

    feat: integrate Sa-Token for authentication and add user controller

--- sa-token-demo-springboot/src/main/java/com/it666/Application.java
+++ sa-token-demo-springboot/src/main/java/com/it666/Application.java
@@ -1,5 +1,6 @@
 package com.it666;
 
+import cn.dev33.satoken.SaManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,6 +13,7 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         System.out.println("启动成功：http://127.0.0.1:8080");
+        System.out.println("启动成功，Sa-Token 配置如下：" + SaManager.getConfig());
     }
 
 }
